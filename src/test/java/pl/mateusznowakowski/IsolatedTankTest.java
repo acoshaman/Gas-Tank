23323a3306417d099c5577d0436c96b90696976c
Mateusz Nowakowski <acoshaman@gmail.com>
2023-04-08 22:09:09 +0200

    Created fields and constructors for IsolatedTank, before testing.

---
+++ src/test/java/pl/mateusznowakowski/IsolatedTankTest.java
@@ -0,0 +1,17 @@
+package pl.mateusznowakowski;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class IsolatedTankTest {
+
+
+    @Test
+    void dbConnectorTest() {
+        new IsolatedTank()
+    }
+
+
+}
